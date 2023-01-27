3a0008c5020c023ce0400627e12f3291531a37d5
s.claes <sebastian.claes@protonmail.com>
2023-01-27 13:03:25 +0100

    [ADD][WIP] tech stack section icons

---
+++ src/components/tab-view/tech-stack-tab/TableRow.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="table-row">
+    <slot name="technology"></slot>
+    <slot name="usage"></slot>
+    <slot name="description"></slot>
+    <slot name="implemented-by"></slot>
+  </div>
+</template>
