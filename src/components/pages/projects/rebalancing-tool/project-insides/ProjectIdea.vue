9d0e4d1c0edb23c05333b4893bd0a21e3e6198f3
s.claes <sebastian.claes@protonmail.com>
2023-02-07 17:39:47 +0100

    [ADD] fully responsive project idea section. (NOTE: still need adjustments after text has been placed)

--- src/components/pages/projects/rebalancing-tool/project-insides/ProjectIdea.vue
+++ src/components/pages/projects/rebalancing-tool/project-insides/ProjectIdea.vue
@@ -3,7 +3,20 @@
 
 <template>
   <div id="project-idea">
-
+    <div class="text-wrapper">
+      <h2 class="heading">The Importance of <span>Rebalancing</span> Your <span>Portfolio</span></h2>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      <a
+          class="button"
+          href="https://suggest-app.com/"
+          title="suggest-app.com"
+      >Download Wireframes</a>
+    </div>
+    <img
+        class="wireframes-bc"
+        src="@/assets/images/Portfolio_Rebalancing_Tool_Wireframes.png"
+        alt="Portfolio Rebalancing Tool Wireframes"
+    />
   </div>
 </template>
 
