40119769838b1f98d94e4072630b5954a6139f7f
s.claes <sebastian.claes@protonmail.com>
2023-02-07 17:45:16 +0100

    [ADD] always start at top on route change

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,14 @@ const router = createRouter({
       name: 'about',
       component: AboutView
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
 
 export default router
