b3576765bdcf4a776601cd99d598580524aebe0f
s.claes <sebastian.claes@protonmail.com>
2023-02-15 23:40:02 +0100

    [ADD] created website router page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProjectPage from '@/components/pages/projects/ProjectPage.vue'
+import WebsitesPage from '@/components/pages/websites/WebsitesPage.vue'
 import AboutPage from '@/components/pages/about/AboutPage.vue'
 import BlogPage from "@/components/pages/blog/BlogPage.vue";
+import {useMainStore} from "@/stores/MainStore";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +12,11 @@ const router = createRouter({
       path: '/',
       name: 'projects',
       component: ProjectPage
+    },
+    {
+      path: '/websites',
+      name: 'websites',
+      component: WebsitesPage
     },
     {
       path: '/about',
@@ -26,6 +33,11 @@ const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     return (savedPosition) ? savedPosition : { top: 0 }
   },
+})
+
+router.beforeEach(() => {
+  const mainStore = useMainStore()
+  mainStore.openNavMenu = false
 })
 
 export default router
