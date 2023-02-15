0b66eb2ae1deb2f0df742f04fb0d35c7c1c8a10a
s.claes <sebastian.claes@protonmail.com>
2023-02-16 00:35:45 +0100

    [ADD] fully functional desktop navbar

--- src/components/content-elements/navbar/DesktopNavbar.vue
+++ src/components/content-elements/navbar/DesktopNavbar.vue
@@ -1,13 +1,30 @@
 <script lang="ts" setup>
 import { RouterLink } from "vue-router";
+import ArrowDownIcon from "@/components/icons/ArrowDownIcon.vue";
+import { useMainStore } from "@/stores/MainStore";
+
+const mainStore = useMainStore()
 </script>
 
 <template>
   <nav id="desktop-navbar">
     <div class="container">
-      <h2>Claes</h2>
+      <h2 ref="heading">Claes</h2>
       <ul>
-        <li><RouterLink :to="{ name: 'projects' }" >PROJECTS</RouterLink></li>
+        <li>
+          <div
+              :class="{active : mainStore.openNavMenu}"
+              class="sub-link"
+              @click="mainStore.openNavMenu = !mainStore.openNavMenu"
+          >
+            <span>PROJECTS</span>
+            <ArrowDownIcon />
+          </div>
+          <div class="sub-link-content" v-show="mainStore.openNavMenu">
+              <RouterLink class="nested-link" :to="{ name: 'projects' }">APPS</RouterLink>
+              <RouterLink class="nested-link" :to="{ name: 'websites' }">WEBSITES</RouterLink>
+          </div>
+        </li>
         <li><RouterLink :to="{ name: 'about' }" >ABOUT ME</RouterLink></li>
         <li><RouterLink :to="{ name: 'blog' }" >BLOG</RouterLink></li>
         <li><a class="contact-btn" href="mailto: s.claes.work@gmail.com">CONTACT</a></li>
