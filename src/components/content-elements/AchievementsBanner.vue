87f79447e64691fb96924176cc502580903c63cd
s.claes <sebastian.claes@protonmail.com>
2023-02-16 18:32:20 +0100

    [NEW] fully responsive achivements wrapper

---
+++ src/components/content-elements/AchievementsBanner.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="achievement-banner">
+    <h3>Achievements</h3>
+
+    <div class="achievement">
+      <div class="number-wrapper">
+        <div class="number">90%</div>
+        <span>more views</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+
+    <div class="achievement">
+      <div class="number-wrapper">
+        <div class="number">90%</div>
+        <span>more views</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+
+    <div class="achievement">
+      <div class="number-wrapper">
+        <div class="number">90%</div>
+        <span>more views</span>
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/content-elements/achievement-banner.scss";
+</style>
--- src/components/pages/websites/WebsitesPage.vue
+++ src/components/pages/websites/WebsitesPage.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import PureAir from "@/components/pages/websites/pure-air/PureAir.vue";
+import AchievementsBanner from "@/components/content-elements/AchievementsBanner.vue";
 </script>
 
 <template>
   <PureAir />
+  <AchievementsBanner :style="{ backgroundColor: '#1E90FF' }"/>
 </template>
